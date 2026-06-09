47639f3a29f146c65bf8bcc4044cd7ae2b36e0e7
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-10 01:52:06 +0500

    Update database.py

--- cogs/database.py
+++ cogs/database.py
@@ -1,1 +1,20 @@
+import discord
+import aiosqlite
+from discord.ext import commands
 
+class Database(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_load(self):
+        # Runs when the Cog is loaded
+        async with aiosqlite.connect("football.db") as db:
+            await db.execute("""
+                CREATE TABLE IF NOT EXISTS predictions 
+                (guild_id TEXT, user_id TEXT, points INTEGER, pick TEXT)
+            """)
+            await db.commit()
+
+async def setup(bot):
+    await bot.add_cog(Database(bot))
+  
