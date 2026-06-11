a4127e483e7d77d8c61b010ce34dce90948e3cda
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-12 00:40:16 +0500

    Delete cogs directory

--- cogs/admin.py
+++
@@ -1,6 +0,0 @@
-@bot.command()
-@commands.is_owner()
-async def sync(ctx):
-    """Syncs slash commands to Discord"""
-    synced = await bot.tree.sync()
-    await ctx.send(f"Synced {len(synced)} command(s) to Discord.")
