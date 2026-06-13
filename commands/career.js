8986b3c2eddcd2f5a1cb973acdbf1a888c99a5ef
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 07:15:00 +0500

    Update career.js

--- commands/career.js
+++ commands/career.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const api = require('../utils/football');
+const api = require('../utils/footballApi');
 module.exports = {
     data: new SlashCommandBuilder().setName('career').setDescription('Player career stats').addStringOption(opt => opt.setName('name').setRequired(true)),
     async execute(interaction) {
