2a0699b934b03937d3ddffbbaef0a4716739f01f
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 06:00:39 +0500

    Add files via upload

--- utils/newsApi.js
+++ utils/newsApi.js
@@ -6,17 +6,30 @@ class NewsAPI {
         this.apiKey = process.env.NEWS_API_KEY;
     }
 
-    async getFootballNews() {
-        const res = await axios.get(`${this.baseURL}/everything`, {
-            params: {
-                q: 'football OR soccer OR "Premier League" OR "Champions League"',
-                language: 'en',
-                sortBy: 'publishedAt',
-                pageSize: 5,
-                apiKey: this.apiKey
-            }
-        });
-        return res.data.articles;
+    async getFootballNews(query = 'football OR "Premier League" OR "Champions League"') {
+        try {
+            const res = await axios.get(`${this.baseURL}/everything`, {
+                params: {
+                    q: query,
+                    language: 'en',
+                    sortBy: 'publishedAt',
+                    pageSize: 5,
+                    apiKey: this.apiKey
+                }
+            });
+            return res.data.articles.filter(a => a.urlToImage); // Only articles with images
+        } catch (e) {
+            console.log('NewsAPI error:', e.message);
+            return [];
+        }
+    }
+
+    async getTransferNews() {
+        return this.getFootballNews('football transfer OR "transfer window" OR "signing"');
+    }
+
+    async getTeamNews(teamName) {
+        return this.getFootballNews(`"${teamName}" football`);
     }
 }
 
