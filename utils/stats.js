c445c9290f472918731ae3b0c15eb0f83894199c
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 06:59:49 +0500

    Delete utils/stats.js

--- utils/stats.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-module.exports = {
-    data: new SlashCommandBuilder().setName('stats').setDescription('Detailed match stats - needs API-Football').addStringOption(opt => opt.setName('match').setRequired(true)),
-    async execute(interaction) {
-        await interaction.reply('Stats require API-Football key. Want me to add it?');
-    }
-};
