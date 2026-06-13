3f28cee5ec0b75295db4a937ccd3d7249353c4d5
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 06:58:24 +0500

    Delete utils/career.js

--- utils/career.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = {
-    data: new SlashCommandBuilder().setName('career').setDescription('Player career stats').addStringOption(opt => opt.setName('name').setRequired(true)),
-    async execute(interaction) {
-        await interaction.reply('Career stats need API-Football. football-data.org free tier doesn\'t have it.');
-    }
-};
