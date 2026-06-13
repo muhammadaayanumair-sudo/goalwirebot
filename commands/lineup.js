f099241c93afe29ff9b05f98bc6fd05e0eced10b
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 07:20:54 +0500

    Update lineup.js

--- commands/lineup.js
+++ commands/lineup.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const api = require('../utils/football');
+const api = require('../utils/footballApi');
 module.exports = {
     data: new SlashCommandBuilder().setName('lineup').setDescription('Match lineups').addStringOption(opt => opt.setName('fixture_id').setDescription('Match ID from /live').setRequired(true)),
     async execute(interaction) {
