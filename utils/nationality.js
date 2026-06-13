61988d6758b33b3dda50183beead3fab15ee1401
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 07:00:20 +0500

    Delete utils/nationality.js

--- utils/nationality.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = {
-    data: new SlashCommandBuilder().setName('nationality').setDescription('Players by country').addStringOption(opt => opt.setName('country').setRequired(true)),
-    async execute(interaction) {
-        await interaction.reply('Nationality search needs API-Football. Want me to add it?');
-    }
-};
