dfb8266010b0e53991c9902dabaad74c69654cf4
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 07:00:09 +0500

    Delete utils/topassist.js

--- utils/topassist.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-module.exports = {
-    data: new SlashCommandBuilder().setName('topassists').setDescription('Top assists - needs API-Football'),
-    async execute(interaction) {
-        await interaction.reply('Assists data requires API-Football. football-data.org only gives scorers.');
-    }
-};
