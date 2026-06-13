cf6b6be0c8fba44d9e8f448e398a5ba913f7b920
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 08:04:33 +0500

    Update card command to view player card collection

--- commands/card.js
+++ commands/card.js
@@ -1,7 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder().setName('cards').setDescription('Most yellow/red cards - needs API-Football').addStringOption(opt => opt.setName('league').setRequired(false)),
+    data: new SlashCommandBuilder()
+        .setName('card')
+        .setDescription('View your Goalwire player card collection')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('View another user\'s card collection') // ← This was likely missing
+                .setRequired(false)),
+    
     async execute(interaction) {
-        await interaction.reply('Card stats require API-Football premium. Not in free APIs.');
+        await interaction.deferReply();
+        const target = interaction.options.getUser('user') || interaction.user;
+        // your card display logic here
+        await interaction.editReply(`${target.username}'s cards will show here`);
     }
 };
