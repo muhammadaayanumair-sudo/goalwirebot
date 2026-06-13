03c32eee40def46ccc6ff50542cf5e5d0077009e
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 07:21:15 +0500

    Rename football utility to footballApi

--- commands/player.js
+++ commands/player.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const api = require('../utils/football');
+const api = require('../utils/footballApi');
 module.exports = {
     data: new SlashCommandBuilder().setName('player').setDescription('Player info').addStringOption(opt => opt.setName('name').setRequired(true)),
     async execute(interaction) {
