59120fee4d97533ddad60ccdc94f657a6717526f
muhammadaayanumair-sudo <muhammadaayanumair@gmail.com>
2026-06-13 07:21:50 +0500

    Update football API import in results command

--- commands/results.js
+++ commands/results.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const api = require('../utils/football');
+const api = require('../utils/footballApi');
 module.exports = {
     data: new SlashCommandBuilder().setName('results').setDescription('Recent results for a team').addStringOption(opt => opt.setName('team').setDescription('Team name').setRequired(true)),
     async execute(interaction) {
